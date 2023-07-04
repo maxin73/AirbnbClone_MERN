29cef9146f2a1826458232a7e442cc366a7cee67
Nami Miyashiro <nami.miyashiro@gmail.com>
2023-07-03 22:36:06 -0700

    first commit

---
+++ client/src/Perks.jsx
@@ -0,0 +1,57 @@
+export default function Perks({selected, onChange}){
+function handleCbClick(ev){
+  const {checked, name} = ev.target;
+  if(checked) {
+    onChange([...selected,name]);
+  } else {
+    onChange([...selected.filter(selectedName => selectedName !== name)]);
+  }
+}
+
+  return(
+    <>
+      <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+        <input type="checkbox" checked={selected.includes('wifi')} name="wifi" onChange={handleCbClick} />
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z" />
+        </svg>
+        <span>Wifi</span>
+      </label>
+      <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+        <input type="checkbox" checked={selected.includes('parking')} name="parking" onChange={handleCbClick} />
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
+        </svg>
+        <span>Free parking spot</span>
+      </label>
+      <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+        <input type="checkbox" checked={selected.includes('tv')} name="tv" onChange={handleCbClick} />
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z" />
+        </svg>
+        <span>TV</span>
+      </label>
+      <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+        <input type="checkbox" checked={selected.includes('pets')} name="pets" onChange={handleCbClick} />
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+        </svg>
+        <span>Pets</span>
+      </label>
+      <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+        <input type="checkbox" checked={selected.includes('entrance')} name="entrance" onChange={handleCbClick} />
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+        </svg>
+        <span>Private entrance</span>
+      </label>
+      <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+        <input type="checkbox" checked={selected.includes('balcony')} name="balcony" onChange={handleCbClick} />
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+        </svg>
+        <span>Balcony</span>
+      </label>
+    </>
+  )
+}
